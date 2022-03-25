f54c9e67fda5f41f55127164fe547782ab9ebd8b
kirtij31 <kirtijain487@gmail.com>
2022-03-25 23:51:11 +0530

    initial commit

--- app/src/main/java/com/example/campusalley/MainActivity.java
+++ app/src/main/java/com/example/campusalley/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.campusalley;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button btn_user, btn_admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_user= findViewById(R.id.btn_user);
+        btn_admin= findViewById(R.id.btn_admin);
+        btn_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it= new Intent(getApplicationContext(),Signin.class);
+                startActivity(it);
+            }
+        });
+        btn_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(),AdminLogin.class);
+                startActivity(it);
+                
+            }
+        });
     }
 }
